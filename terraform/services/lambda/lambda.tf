1d904b5bd8170a4ae1129c7a9a55e57c831fb9d6
David_Hackett <davidhackett36@gmail.com>
2021-10-13 15:59:56 +0700

    chore(terraform): Update lambda version

--- terraform/services/lambda/lambda.tf
+++ terraform/services/lambda/lambda.tf
@@ -1,6 +1,6 @@
 module "handle_slack_request_lambda" {
   source            = "./function"
   name              = "handle-slack-request"
-  lambda_version    = "0.1.0"
+  lambda_version    = "0.1.3"
   deployment_bucket = var.deployment_bucket
 }
